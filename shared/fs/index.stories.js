12f8d78b1b145c48bc2df2f171b5308c146163e6
Taru Karttunen <taruti@taruti.net>
2018-02-07 22:12:07 +0200

    Add fs/filepreview to storybook

--- shared/fs/index.stories.js
+++ shared/fs/index.stories.js
@@ -4,6 +4,7 @@ import * as Types from '../constants/types/fs'
 import {Box} from '../common-adapters'
 import {storiesOf} from '../stories/storybook'
 import Files from '.'
+import FilePreview from './filepreview'
 
 const load = () => {
   storiesOf('Files', module).add('Root', () => (
@@ -11,6 +12,8 @@ const load = () => {
       <Files path={Types.stringToPath('/keybase')} items={[]} onBack={() => {}} />
       <Files path={Types.stringToPath('/keybase/private')} items={[]} onBack={() => {}} />
       <Files path={Types.stringToPath('/keybase/public')} items={[]} onBack={() => {}} />
+      <FilePreview path={'/keybase/private/foo/bar.img'}
+      meta={{lastModifiedTimestamp: 1518029754000, size: 15000, lastWriter: 'foobar', progress: 'pending'}}/>
     </Box>
   ))
 }
